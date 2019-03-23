3795f3912b0e80bfdb15519f3a5e2a34b782e711
Benjamin Trounson <BeatrixTrounson@github.com>
2019-03-24 00:46:48 +1300

    first commit
    The Boii's Bot

---
+++ index.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+const { Client, Attachment } = require('discord.js');
+const client = new Discord.Client();
+
+ /**
+  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+  * received from Discord
+  */
+ client.on('ready', () => {
+   console.log('I am ready!');
+ });
+
+ // Create an event listener for messages
+ client.on('message', message => {
+   // If the message is "ping"
+   if (message.content === 'bitch') {
+     // Send "pong" to the same channel
+     message.channel.send('lasanga');
+   }
+ });
+
+ // Create a new webhook
+const hook = new Discord.WebhookClient('558921382407307275', 'amj-kkWkIjg6GMNT8ds1L0WNAI8J1e7AC2wnF9hu6H6OX79A8vc3rd541Tr7iDpxUtxf');
+
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+});
+
+client.on('message', message => {
+    // If the message is '!rip'
+    if (message.content === '!rip') {
+        // Create the attachment using Attachment
+        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+        // Send the attachment in the message channel
+        message.channel.send(attachment);
+    }
+});
+
+/**
+ * A bot that welcomes new guild members when they join
+ */
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
+// Send a message using the webhook
+hook.send('I am now alive!');
+
+client.login('NTU4OTA4MzU5Mjg2NDU2MzIx.D3dwFA.J2qUQ7g9H5JhNWWvNii6xAQNdO0');
---
+++ package.js
@@ -0,0 +1,8 @@
+{
+“name”: “greeter-bot”,
+“version”: “1.0.0”,
+“description”: “The Boii's Bot”,
+“main”: “index.js”,
+“author”: “Benjamin Trounson”,
+“dependencies”: {}
+}
